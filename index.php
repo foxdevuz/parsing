b0d1c17181a6e496e7b7857c42ea6e2929641fcc
Foxdevuz <abdullajonovboburjon05@gmail.com>
2023-06-18 06:26:35 +0500

    youtube download site parsed

--- index.php
+++ index.php
@@ -2,11 +2,18 @@
     // Include the library
     include './simplehtmldom_1_9_1/simple_html_dom.php';
 
-    $target = "https://store.steampowered.com/";
+    $video = "https://youtu.be/HxXDpdhAVbY";
+    $target = "https://10downloader.com/download?v=".$video."&utm_source=000tube";
     $req = file_get_html($target);
 
-    foreach ($req->find('img') as $value) {
+    foreach ($req->find('a[class=downloadBtn]') as $value) {
         // echo "<img style='width:100px;' src=".$value->src."/>";
-        echo $value->src . "<br/>";
+        // echo '
+        //     <video width="320" height="240" controls>
+        //         <source src="'.$value->href.'">
+        //     </video>
+        // ';
+        echo $value->href. "<br/> <br/>";
     }
+
 ?>
